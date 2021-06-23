5a1524565f2333a8fa02f866933e0cdf30b1c632
Lynne16 <76992610+Lynne16@users.noreply.github.com>
2021-06-23 14:36:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/WAY8pLEs-/model.json',modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
 }
 
 function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
 
 }
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);   
+     }
+     else{
+         console.log(results);
+         document.getElementById('result_person_name').innerHTML=results[0].label;
+         document.getElementById('result_accu').innerHTML=results[0].confidence.toFixed(3);
+     }
+ 
+ }
